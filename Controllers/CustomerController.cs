f293afc401da84e6a3fd12a500cca84e64b187bf
Muhammet-Dobaoglu <muhammetdobaoglu@hotmail.com>
2023-03-10 14:38:44 +0300

    Add project files.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using RentACar.BusinessLayer.Concrete;
+using RentACar.DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentACar.PresentationLayer.Controllers
+{
+    public class CustomerController : Controller
+    {
+        CustomerManager customerManager  = new CustomerManager(new EfCustomerDal());
+        public ActionResult Index()
+        {
+            var values = customerManager.TGetList();
+            return View(values);
+        }
+
+
+    }
+}
